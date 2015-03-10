5e8f8b1e41aab6c2fbd5e40606076e51ad22ee17
bethmingledorff <beth.mingledorff@gmail.com>
2015-03-10 10:10:59 -0500

    hmwrk part2

---
+++ part2.rb
@@ -0,0 +1,17 @@
+print "How's the weather today? "
+
+input = gets.chomp
+
+	if input == "sunny"
+		puts "Do your homework. Then go outside!"
+
+	elsif input == "warm"
+		puts "That's nice, but your homework's not done yet. Do your homework."
+
+	elsif input == "rainy"
+		puts "It's a good day to do homework...:-/"
+
+	else
+		puts "Doesn't matter. DO YOUR HOMEWORK."
+
+	end		
