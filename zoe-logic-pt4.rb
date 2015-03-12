122769b40edae6cc6957e4ff9e3897feb5b29444
zpalmer <zoepalmer2014@u.northwestern.edu>
2015-03-12 14:04:50 -0500

    fixed up part 4 to make it more streamlined

--- zoe-logic-pt4.rb
+++ zoe-logic-pt4.rb
@@ -2,25 +2,14 @@
 # (one word per line, continuing until we just press Enter on an empty line),
 # and which then repeats the words back to us in reverse alphabetical order
 
-
-# def reverse_alphabetize(array_of_words)
-# 	return new_array_of_words = array_of_words.sort.reverse
-# end
-
 def reverse_alphabetize()
 	array_of_words = []
 	puts "Enter a word: "
-	word = gets.chomp
-	while word != ""
+	while (word = gets.chomp) != ""
 		array_of_words.push(word)
-		word = gets.chomp
 	end
 	puts "Re-alphabetizing..."
-	array_of_words.map! {|word| word.downcase}
-	# array_of_words.sort!
-	# while array_of_words != []
-	# 	puts array_of_words.pop + "\n"
-	# end
+	array_of_words.map! {|word| word.downcase} # stop caps from messing up the order
 	array_of_words.sort.reverse.each {|word| puts word}
 end
 
