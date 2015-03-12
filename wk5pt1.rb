20a31c8f7294bd68ac8e6d42b21c5456070926e2
kellitjones <jonestkelli@gmail.com>
2015-03-11 21:04:46 -0500

    part one of homework

---
+++ wk5pt1.rb
@@ -0,0 +1,15 @@
+def odiepoots(x)
+	if x > 10
+		puts "WHOA"
+	elsif x <= 10 && x !=-3
+		puts "whoaaaa"	
+	elsif x > 14 && x < 100 && x % 2 == 0
+		puts "Penguin"
+	elsif x >= 100 && x<= 1000 || x % 2 !=0
+		puts "Giraffe"
+	 else
+	 	puts "Caballo"
+	 end
+end
+
+
