4fee24f1fa90a89ffb629585014823685939eec7
zpalmer <zoepalmer2014@u.northwestern.edu>
2015-03-12 14:20:35 -0500

    getting ready to fix part 1 to account for meeting two conditions.

--- zoe-logic-pt1.rb
+++ zoe-logic-pt1.rb
@@ -3,20 +3,36 @@
 # 1. Output "WHOA" if x is greater than 10
 # 2. Output "whoaaaa" if x is less than or equal to 10, but not negative
 # 3. If x is greater than 14 but less than 100, and x is even, output "Penguin".
-# 4. If it is greater than or equal to 100 and it is less than or equal to 1000,
-#  or, if x is odd, then output "Giraffe".
+# 4. If x is greater than or equal to 100 and it is less than or equal to 1000,
+#  		or, if x is odd, then output "Giraffe".
 # 5. Otherwise, output "Caballo"
 
 def logic1(x)
+	output = []
 	if x > 10
 		return "WHOA"
-	elsif 0 <= x <= 10
+	elsif 0 <= x && x <= 10
 		return "whoaaaa"
-	elsif 14 < x < 100 && x % 2 == 0
+	elsif 14 < x && x < 100 && x % 2 == 0
 		return "Penguin"
-	elsif x % 2 != 0:
+	elsif 100 <= x && x <= 1000 && x % 2 != 0
 		return "Giraffe"
 	else
 		return "Caballo"
 	end
 end
+
+puts "11:"
+puts logic1(11)
+puts "10:"
+puts logic1(10)
+puts "2:"
+puts logic1(2)
+puts "16:"
+puts logic1(16)
+puts "23:"
+puts logic1(23)
+puts "102:"
+puts logic1(102)
+puts "10001:"
+puts logic1(10001)
